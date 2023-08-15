948feaa2f8ca42fd5485154e3dee2f1ec0307764
Gui <guilherme.ssoares13@hotmail.com>
2023-08-15 20:12:03 -0300

    Primeira Parcial

---
+++ Guilhermesoaresado1/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Gorila animal1 = new Gorila();
+        animal1.setnome("Carlinhos do pão");
+        animal1.setIdade(50);
+        animal1.setCor("Pretão");
+        
+
+        Gorila animal2 = new Gorila();
+
+    }
+}
