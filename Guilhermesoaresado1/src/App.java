64becb6ed62e389e387198241837feac5a17b315
Gui <guilherme.ssoares13@hotmail.com>
2023-08-25 21:05:36 -0300

    Final Version

--- Guilhermesoaresado1/src/App.java
+++ Guilhermesoaresado1/src/App.java
@@ -2,14 +2,20 @@ public class App {
 
     public static void main(String[] args){
 
-        Gorila animal1 = new Gorila();
+        Gorila animal1 = new Gorila("Jean Willys" , "Azul" , 8);
         System.out.printf("Nome do primeiro gorila: %s \n\n" , animal1.getnome());
         
         Gorila animal2 = new Gorila("Angel Romero", 
                                      "Rosão",
                                      18);
                                   
-        System.out.printf("Nome do  Segundo Gorila: %s" , animal2.getnome());
-        
+        System.out.printf("Nome do  Segundo Gorila: %s \n\n" , animal2.getnome());
+
+
+            
+            animal2.comer();
+            
+            animal2.andar();
+
     }
 }
--- Guilhermesoaresado1/src/Gorila.java
+++ Guilhermesoaresado1/src/Gorila.java
@@ -1,81 +1,63 @@
 public class Gorila {
-    
+
     // atributos!
-    private String nome;
+    private static String nome;
     private String cor;
     private int idade;
-    int passo; 
-    int comer;
+    
 
-    // construtores 
+    // construtores
 
     public Gorila (){
-        nome = "Jean Willys";
+
     }
 
-    public Gorila (String nome, String cor, int idade){
+    public Gorila(String nome, String cor, int idade) {
         this.nome = nome;
         this.cor = cor;
         this.idade = idade;
-    }  
+    }
 
-    public int getPasso() {
-        return passo;
-    }
-    public void setPasso(int passo) {
-        this.passo = passo;
-    }
-    public int getComer() {
-        return comer;
-    }
-    public void setComer(int comer) {
-        this.comer = comer;
-    }
 
     // métodos acessores!
 
-    public String getnome() {
+    public static String getnome() {
         return nome;
     }
+
     public void setnome(String nome) {
         this.nome = nome;
     }
+
     public String getCor() {
         return cor;
     }
+
     public void setCor(String cor) {
         this.cor = cor;
     }
+
     public int getIdade() {
         return idade;
     }
+
     public void setIdade(int idade) {
         this.idade = idade;
     }
 
-    // ações e construtores!
+    // ações!
 
-    public int andar (){
-        this.passo += 1;
-        return passo;
+    public static void comer() {
+
+        System.out.printf("O %s está comendo!\n", getnome());
+
     }
 
-    public int andar (int novoPasso){
-        this.passo += novoPasso;
-        return passo;
+    public static void andar() {
+
+        System.out.printf("\n%s está andando!" , getnome());
+
+        
     }
-
-    public int alimento (){
-        this.comer += 1;
-        return comer;
-    }
-
-    public int alimento (int comer1){
-        this.comer += comer1;
-        return comer;
-    }
-
-   
-
 
 }
